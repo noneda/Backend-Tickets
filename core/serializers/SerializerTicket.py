eedc2b132a636c024328220915dce326fca0a99a
noneda <nonejda@gmail.com>
2025-08-01 12:58:25 -0500

    feat: Add completeDate field to SerializerTicket for improved date handling

--- core/serializers/SerializerTicket.py
+++ core/serializers/SerializerTicket.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 class SerializerTicket(serializers.ModelSerializer):
     typeTicket = serializers.CharField(source="typeTicket.name")
     submissionDate = serializers.DateTimeField(format="%Y/%m/%d")
+    completeDate = serializers.DateTimeField(format="%Y/%m/%d")
 
     class Meta:
         model = Ticket
